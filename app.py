748992003fa0f033b1955cc8adb14d59d9f7e956
NoeChavezVega <a377347@uach.mx>
2025-11-17 22:29:37 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,6 @@ Progreso={"Energia Solar":{"Echo": False, "Puntaje":0},
           "Energia Hidraulica":{"Echo": False,"Puntaje":0},
           "Energia por Biomasa":{"Echo": False,"Puntaje":0},}
 Lecciones_totales:len(Progreso)
+def mostrar_dashboard():
+    """Pantalla Principal: Dashboard del Estudiante."""
+    st.header("🌱 EcoAprende: Tu Aventura Ecológica")
