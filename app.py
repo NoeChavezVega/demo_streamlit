18b4eb40dd75f4cb2a25cb23a07924f69fd25ea2
NoeChavezVega <a377347@uach.mx>
2025-11-17 22:19:56 -0600

    Fix typo in progreso variable name

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 st.title("⚡EcoAprende🌱")
 
-progreso={"Energia Solar":{"Echo": False, "Puntaje":0},
+Progreso={"Energia Solar":{"Echo": False, "Puntaje":0},
           "Energia Eolica":{"Echo": False,"Puntaje":0},
           "Energia Hidraulica":{"Echo": False,"Puntaje":0},
           "Energia por Biomasa":{"Echo": False,"Puntaje":0},}
+Lecciones_totales:len(Progreso)
