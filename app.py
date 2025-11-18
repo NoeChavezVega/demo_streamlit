eabd11321c8dc7c7a28a0979870095bada5e9d1b
NoeChavezVega <a377347@uach.mx>
2025-11-17 23:17:50 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 import streamlit as st
 st.title("⚡EcoAprende🌱")
 
-Progreso={"Energia Solar":{"Hecho": False, "Puntaje":0},
+progreso={"Energia Solar":{"Hecho": False, "Puntaje":0},
           "Energia Eolica":{"Hecho": False,"Puntaje":0},
           "Energia Hidraulica":{"Hecho": False,"Puntaje":0},
           "Energia por Biomasa":{"Hecho": False,"Puntaje":0},}
-Juegos_totales:len(Progreso)
+Juegos_totales:len(progreso)
 def mostrar_dashboard():
-          """Patnalla Principal:Daschboard del Jugador"""
-          Juegos_completadas = sum(1 for data in progreso.values() if data["Hecho"])
-    insignias = Juegos_completados
-Juegos_completados=sum(1 for data in progreso.values() if data["Hecho"])
+          """Pantalla Principal: Dashboard del Jugador."""
+
+Juegos_completados = sum(1 for data in progreso.values() if data["Hecho"])
+Logros = Juegos_completados
+
 
           
 
