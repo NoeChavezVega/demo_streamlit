8cea16edf6ea3fc6c40e492a587efafd6f23b148
NoeChavezVega <a377347@uach.mx>
2025-11-17 22:06:31 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 st.title("⚡EcoAprende🌱")
-
+progreso = {
+    "Solar": {"completado": False, "puntaje": 0},
+    "Eolica": {"completado": False, "puntaje": 0},
+    "Hidraulica": {"completado": False, "puntaje": 0},
+    "Biomasa": {"completado": False, "puntaje": 0}
