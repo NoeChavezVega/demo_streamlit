dc0f7b2ff3ea68e03874acaadf137d3b1592df23
NoeChavezVega <a377347@uach.mx>
2025-11-17 23:10:11 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ Progreso={"Energia Solar":{"Hecho": False, "Puntaje":0},
           "Energia por Biomasa":{"Hecho": False,"Puntaje":0},}
 Juegos_totales:len(Progreso)
 def mostrar_dashboard():
-          "Patnalla Principal:Daschboard del Jugador"
+          """Patnalla Principal:Daschboard del Jugador"""
           Juegos_completadas = sum(1 for data in progreso.values() if data["Hecho"])
     insignias = Juegos_completados
 Juegos_completados=sum(1 for data in progreso.values() if data["Hecho"])
