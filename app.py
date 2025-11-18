dc2ac977d691c27a9167b6299539e6fca1fbf7fc
NoeChavezVega <a377347@uach.mx>
2025-11-17 23:34:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
 import streamlit as st
 st.title("⚡EcoAprende🌱")
+progreso = {
+    "Energia Solar": {"Hecho": False, "Puntaje": 0},
+    "Energia Eolica": {"Hecho": False, "Puntaje": 0},
+    "Energia Hidraulica": {"Hecho": False, "Puntaje": 0},
+    "Energia por Biomasa": {"Hecho": False, "Puntaje": 0},}
 
-progreso={"Energia Solar":{"Hecho": False, "Puntaje":0},
-          "Energia Eolica":{"Hecho": False,"Puntaje":0},
-          "Energia Hidraulica":{"Hecho": False,"Puntaje":0},
-          "Energia por Biomasa":{"Hecho": False,"Puntaje":0},}
-Juegos_totales:len(progreso)
+Juegos_totales = len(progreso)
 
 def mostrar_dashboard():
-          """Pantalla Principal: Dashboard del Jugador."""
+    """Pantalla Principal: Dashboard del Jugador."""
 
-Juegos_completados = sum(1 for data in progreso.values() if data["Hecho"])
-st.header("EcoAprende - Dashboard")
-st.write(f"Progreso general: {Juegos_completados} / {Juegos_totales}")
+    Juegos_completados = sum(1 for data in progreso.values() if data["Hecho"])
 
-st.subheader("Lecciones disponibles:")
+    st.header("EcoAprende - Dashboard")
+    st.write(f"Progreso general: {Juegos_completados} / {Juegos_totales}")
 
- for juego in progreso.keys():
-           if st.button(juego):
-                     st.session_state["pantalla_actual"]=juego
+    st.subheader("Lecciones disponibles:")
+
+    for juego in progreso.keys():
+        if st.button(juego):
+            st.session_state["pantalla_actual"] = juego
+
 
 
 
