b5774acc7be18bd63eaaae958c7392bee37904fc
NoeChavezVega <a377347@uach.mx>
2025-11-18 11:56:31 -0600

    Fix formatting and syntax in app.py

--- app.py
+++ app.py
@@ -5,23 +5,17 @@ progreso = {
     "Solar": {"completado": False, "puntaje": 0},
     "Eolica": {"completado": False, "puntaje": 0},
     "Hidraulica": {"completado": False, "puntaje": 0},
-    "Biomasa": {"completado": False, "puntaje": 0},
-}
-
+    "Biomasa": {"completado": False, "puntaje": 0},}
 total_juegos = len(progreso)
-
 def mostrar_dashboard():
     st.header("🌱 EcoAprende - Dashboard de Juegos")
-
     juegos_completados = sum(1 for data in progreso.values() if data["completado"])
     st.write(f"Progreso general: **{juegos_completados}/{total_juegos}** juegos completados")
-
     st.subheader("Selecciona un juego:")
 
     for juego in progreso.keys():
         if st.button(juego):
             st.session_state["pantalla"] = juego
-
 #Energia solar
 def juego_solar():
     st.title("🌞 Juego: Energía Solar")
@@ -30,13 +24,11 @@ def juego_solar():
 
     p1 = st.radio(
         "¿Qué energía solar genera electricidad?",
-        ["Solar Térmica", "Solar Fotovoltaica", "Solar Geotérmica"]
-    )
+        ["Solar Térmica", "Solar Fotovoltaica", "Solar Geotérmica"])
 
     p2 = st.radio(
         "¿Cuál es el principal beneficio ambiental?",
-        ["Genera pocos residuos", "Reduce CO2", "Funciona de noche"]
-    )
+        ["Genera pocos residuos", "Reduce CO2", "Funciona de noche"])
 
     if st.button("Enviar respuestas"):
         puntaje = 0
